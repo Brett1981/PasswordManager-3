57ad3e2dd08e7fb2be560cd0e7135ff557b51e03
AhOnela1 <arnaud.lassartesse@epita.fr>
2021-04-27 18:14:22 +0200

    [MOD]Database increment index account table

---
+++ PasswordManager/Database/dbo/Tables/Account_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Account] (
+    [id]       INT           IDENTITY (1, 1) NOT NULL,
+    [name]     NVARCHAR (50) NULL,
+    [login]    NVARCHAR (50) NULL,
+    [password] NVARCHAR (50) NULL,
+    CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
