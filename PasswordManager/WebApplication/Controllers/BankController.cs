b8a0ed58c95f7e76ef032bce5a033f87e3a1ab19
Kenex <kenedjq@gmail.com>
2021-05-03 21:19:52 +0200

    bank

---
+++ PasswordManager/WebApplication/Controllers/BankController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class BankController : Controller
+    {
+        private readonly DataAccess.Interfaces.IBankRepository _bankRepository;
+        BankViewModel bankViewModel;
+
+
+        public BankController(DataAccess.Interfaces.IBankRepository bankRepository)
+        {
+            bankViewModel = new BankViewModel();
+            _bankRepository = bankRepository;
+            
+        }
+        /*
+        public ActionResult Banks()
+        {
+            
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(AccountViewModel accountViewModel)
+        {
+           
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(AccountViewModel accountViewModel)
+        {
+            return RedirectToAction("Accounts", "Account");
+        }*/
+    }
+}
