d6576cda355cb716b7597dc41e7d41db419816b1
AhOnela1 <arnaud.lassartesse@epita.fr>
2021-05-06 17:17:26 +0200

    [FEATURE]Password generator

---
+++ PasswordManager/WebApplication/Controllers/PasswordController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Crypto.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.DataAccess;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class PasswordController : Controller
+    {
+        public PasswordController()
+        {
+        }
+
+        public ActionResult Generate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Generate(PasswordViewModel passwordViewModel)
+        {
+            var chars = "abcdefghijklmnopqrstuvwxyz";
+            
+            if (passwordViewModel.Uppercase)
+                chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            if (passwordViewModel.Number)
+                chars += "0123456789";
+
+            if (passwordViewModel.Symbol)
+                chars += "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
+
+            Random random = new Random();
+            var result = "";
+
+            for (int i = 0; i < 12; i++)
+            {
+                char c = chars[random.Next(chars.Length)];
+                result += c;
+            }
+
+            ModelState.Clear();
+            passwordViewModel.Password = result;
+
+            return View(passwordViewModel);
+        }
+    }
+}
---
+++ PasswordManager/WebApplication/Models/PasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class PasswordViewModel
+    {
+        public string Password { get; set; }
+        [Display(Name = "Allow uppercases")]
+        public bool Uppercase { get; set; }
+        [Display(Name = "Allow numbers")]
+        public bool Number { get; set; }
+        [Display(Name = "Allow symbols")]
+        public bool Symbol { get; set; }
+    }
+}
