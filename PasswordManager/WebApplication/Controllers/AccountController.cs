a329e2a6e89e8748350a616d0cf8024dc6e2c552
AhOnela1 <arnaud.lassartesse@epita.fr>
2021-05-07 12:57:38 +0200

    [FIX]Fixed passwords options + Fixed password encryption and decryption

--- PasswordManager/WebApplication/Controllers/AccountController.cs
+++ PasswordManager/WebApplication/Controllers/AccountController.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using Org.BouncyCastle.Crypto.Generators;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using WebApplication.DataAccess;
 using WebApplication.Models;
@@ -16,6 +19,7 @@ namespace WebApplication.Controllers
         private readonly DataAccess.Interfaces.ICategoryRepository _categoryRepository;
         AccountViewModel accountViewModel;
         public static int sessionId = 0;
+        public static string encryptionKey = "VWF4FZqVHW7l5lvoazeQ";
 
         public AccountController(DataAccess.Interfaces.IAccountRepository accountRepository, DataAccess.Interfaces.ICategoryRepository categoryRepository)
         {
@@ -33,6 +37,9 @@ namespace WebApplication.Controllers
             var unattachedAccounts = accounts.Where(x => x.CategoryId == null).ToList();
             var categories = _accountRepository.GetCategoriesBySessionId(sessionId);
 
+            accounts.ForEach(x => x.Password = Decrypt(x.Password, encryptionKey));
+            unattachedAccounts.ForEach(x => x.Password = Decrypt(x.Password, encryptionKey));
+
             ViewBag.Accounts = checkCompromisedPasswords(accounts).Result;
             ViewBag.Unattached = checkCompromisedPasswords(unattachedAccounts).Result;
             ViewBag.Categories = categories;
@@ -49,7 +56,7 @@ namespace WebApplication.Controllers
             var account = new Dbo.Account();
             account.Name = accountViewModel.Name;
             account.Login = accountViewModel.Login;
-            account.Password = accountViewModel.Password;
+            account.Password = Encrypt(accountViewModel.Password, encryptionKey);
             if (!String.IsNullOrEmpty(accountViewModel.Url))
                 account.Url = "http://" + accountViewModel.Url;
             account.Name = accountViewModel.Name;
@@ -83,7 +90,7 @@ namespace WebApplication.Controllers
             var account = _accountRepository.GetById(accountViewModel.Id);
             account.Name = accountViewModel.Name;
             account.Login = accountViewModel.Login;
-            account.Password = accountViewModel.Password;
+            account.Password = Encrypt(accountViewModel.Password, encryptionKey);
             if (!String.IsNullOrEmpty(accountViewModel.Url))
                 account.Url = "http://" + accountViewModel.Url;
             account.Name = accountViewModel.Name;
@@ -128,7 +135,7 @@ namespace WebApplication.Controllers
             accountViewModel.Id = account.Id;
             accountViewModel.Name = account.Name;
             accountViewModel.Login = account.Login;
-            accountViewModel.Password = account.Password;
+            accountViewModel.Password = Decrypt(account.Password, encryptionKey);
             accountViewModel.Url = account.Url.Substring(7);
 
             if (account.CategoryId != null)
@@ -160,6 +167,9 @@ namespace WebApplication.Controllers
                 var categories = _accountRepository.GetCategoriesBySessionId(sessionId);
                 var filteredCategories = categories.Where(x => filteredAccounts.Any(y => y.CategoryId == x.Id)).Distinct().ToList();
 
+                filteredAccounts.ForEach(x => x.Password = Decrypt(x.Password, encryptionKey));
+                unattachedAccounts.ForEach(x => x.Password = Decrypt(x.Password, encryptionKey));
+
                 ViewBag.Accounts = checkCompromisedPasswords(filteredAccounts).Result;
                 ViewBag.Unattached = checkCompromisedPasswords(unattachedAccounts).Result;
                 ViewBag.Categories = filteredCategories;
@@ -170,12 +180,59 @@ namespace WebApplication.Controllers
                 var categories = _accountRepository.GetCategoriesBySessionId(sessionId);
                 var filteredCategories = categories.Where(x => x.Name.ToLower().Contains(accountViewModel.Search.Value.ToLower())).ToList();
 
+                accounts.ForEach(x => x.Password = Decrypt(x.Password, encryptionKey));
+
                 ViewBag.Accounts = checkCompromisedPasswords(accounts).Result;
                 ViewBag.Unattached = new Dictionary<Dbo.Account, string>();
                 ViewBag.Categories = filteredCategories;
             }
 
             return View();
+        }
+
+        public string Encrypt(string text, string encryptionKey)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(text);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(encryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytes, 0, bytes.Length);
+                        cs.Close();
+                    }
+                    text = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return text;
+        }
+
+        public string Decrypt(string encryptedText, string encryptionKey)
+        {
+            byte[] bytes = Convert.FromBase64String(encryptedText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(encryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytes, 0, bytes.Length);
+                        cs.Close();
+                    }
+                    encryptedText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+
+            return encryptedText;
         }
 
         public async Task<Dictionary<Dbo.Account, string>> checkCompromisedPasswords(List<Dbo.Account> accounts)
